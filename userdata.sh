db812d7f4b6d94dbaccd23794905329f24df8fc0
fevzibayrak <117108955+fevzibayrak@users.noreply.github.com>
2023-03-23 00:21:55 +0300

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-hostnamectl set-hostname ${server-name}
-yum update -y
-amazon-linux-extras install docker -y
-systemctl start docker
-systemctl enable docker
-usermod -a -G docker ec2-user
+sudo yum update -y
+sudo yum install docker
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo systemctl status docker
+sudo usermod -a -G docker ec2-user
+newgrp docker
 # install docker-compose
 curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" \
 -o /usr/local/bin/docker-compose
